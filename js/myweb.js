57803abab73230fdb9c22b91fcac9b0f50c7c334
Arsalan <arsalanahmedaa3165792@gmail.com>
2025-05-12 21:15:41 +0500

    abc

---
+++ js/myweb.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function(){
+$(window).scroll(function(){
+
+   $(window).scrollTop()  
+
+  if($(window).scrollTop() > 100){
+     $("nav").addClass("after")
+  }
+  else{
+    $("nav").removeClass("after")
+  }
+
+})  
+
+$(window).scrollTop(function(){
+  
+  if($(window).scrollTop()>200){
+      $("#scrol-btn").addClass("fix-btn")
+  }
+  
+})
+
+
+})
+
+
+function toggleMenu() {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.classList.toggle('active');
+}
+
+AOS.init({
+  offset: 1500,
+  once: true,
+});
