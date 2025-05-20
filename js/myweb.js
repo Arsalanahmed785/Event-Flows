1e8f3d6184fe027cd40265e7a5b4f31fde93a09b
Arsalan <arsalanahmedaa3165792@gmail.com>
2025-05-20 18:19:12 +0500

    update style1.css, index.html and myweb.js

--- js/myweb.js
+++ js/myweb.js
@@ -13,17 +13,28 @@ $(window).scroll(function(){
 
 })  
 
-$(window).scrollTop(function(){
+// $(window).scrollTop(function(){
   
-  if($(window).scrollTop()>400){
-      $("#scroll-btn").addClass("fix-btn")
-      $("#scroll-btn").addClass("fix-icon")
+//   if($(window).scrollTop()>400){
+//       $("#scroll-btn").addClass("fix-btn")
+//       $("#scroll-btn").addClass("fix-icon")
+//   }
+  
+// })
+
+
+})
+$(window).on("scroll", function() {
+  if ($(this).scrollTop() > 400) {
+    $("#scroll-btn").fadeIn();
+  } else {
+    $("#scroll-btn").fadeOut();
   }
-  
-})
-
-
-})
+});
+$("#scroll-btn").on("click", function(e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, "slow");
+});
 
 
 function toggleMenu() {
